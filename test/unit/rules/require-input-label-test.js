685deb56a66bfad06d4ca0a3d1a99e33b3903c60
Steve Zelaznik <steve.zelaznik@iorahealth.com>
2020-09-29 20:22:13 +0000

    add <textarea> support to require-input-label
    A <textarea> component is still an input, and the same
    labelling WCAG rules apply.

--- test/unit/rules/require-input-label-test.js
+++ test/unit/rules/require-input-label-test.js
@@ -33,6 +33,24 @@ generateRuleTests({
     '<input type="hidden"/>',
     '<Input type="hidden" />',
     '{{input type="hidden"}}',
+
+    // Same logic applies to textareas
+    '<label>LabelText<textarea /></label>',
+    '<label><textarea />LabelText</label>',
+    '<label>LabelText<Textarea /></label>',
+    '<label><Textarea />LabelText</label>',
+    '<label>Label Text<div><textarea /></div></label>', // technically okay, hopefully no one does this though
+    '<textarea id="probablyHasLabel" />', // it's likely to have an associated label if it has an id attribute
+    '<textarea aria-label={{labelText}} />',
+    '<textarea aria-labelledby="someIdValue" />',
+    '<div></div>',
+    '<textarea ...attributes />', // we are unable to correctly determine if this has a label or not, so we have to allow it
+    '<Textarea ...attributes />',
+    '<Textarea id="foo" />',
+    '{{textarea id="foo"}}',
+    '<label>Text here<Textarea /></label>',
+    '<label>Text here {{textarea}}</label>',
+    '<textarea id="label-textarea" ...attributes />',
   ],
 
   bad: [
@@ -169,6 +187,87 @@ generateRuleTests({
         line: 1,
         column: 0,
         source: '<Input type={{myType}}/>',
+      },
+    },
+    {
+      template: '<div><textarea /></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 5,
+        source: '<textarea />',
+      },
+    },
+    {
+      template: '<textarea />',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 0,
+        source: '<textarea />',
+      },
+    },
+    {
+      template: '<textarea title="some title value" />',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 0,
+        source: '<textarea title="some title value" />',
+      },
+    },
+    {
+      template: '<label><textarea /></label>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 7,
+        source: '<textarea />',
+      },
+    },
+    {
+      template: '<div>{{textarea}}</div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 5,
+        source: '{{textarea}}',
+      },
+    },
+    {
+      template: '<Textarea/>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 0,
+        source: '<Textarea/>',
+      },
+    },
+    {
+      template: '<textarea aria-label="first label" aria-labelledby="second label" />',
+      result: {
+        message: ERROR_MESSAGE_MULTIPLE_LABEL,
+        line: 1,
+        column: 0,
+        source: '<textarea aria-label="first label" aria-labelledby="second label" />',
+      },
+    },
+    {
+      template: '<textarea id="label-textarea" aria-label="second label" />',
+      result: {
+        message: ERROR_MESSAGE_MULTIPLE_LABEL,
+        line: 1,
+        column: 0,
+        source: '<textarea id="label-textarea" aria-label="second label" />',
+      },
+    },
+    {
+      template: '<label>Textarea label<textarea aria-label="Custom label" /></label>',
+      result: {
+        message: ERROR_MESSAGE_MULTIPLE_LABEL,
+        line: 1,
+        column: 21,
+        source: '<textarea aria-label="Custom label" />',
       },
     },
   ],
